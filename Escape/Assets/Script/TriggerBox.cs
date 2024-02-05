f8e2e9e3566e33a69bbd3b8c0f5628bf4b1606b4
Loane BECEL <loane.becel@ingerop.com>
2024-02-05 21:35:18 +0100

    Ajout des collider+clésUSB

---
+++ Escape/Assets/Script/TriggerBox.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBox : MonoBehaviour
+{
+    public Transform chaise;
+    public Transform ordi;
+    public GameObject perso;
+    public GameObject livreOuvert;
+    public GameObject livreFerme;
+    public Transform PanneauCles;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "chaise":
+                perso.transform.position = col.transform.position + 5 * Vector3.up;
+                perso.transform.LookAt(ordi);
+                break;
+            case "box":
+                perso.transform.position = new Vector3(-5, 12.5f, -33);
+                perso.transform.rotation = Quaternion.Euler(new Vector3(33.5f,-86,-7));
+                break;
+            case "livre":
+                livreOuvert.SetActive(true);
+                livreFerme.SetActive(false);
+                break;
+            case "clé":
+                col.transform.position = new Vector3(-18.1f, 10.15f, -30);
+                break;
+            case "cléMort":
+                col.transform.position = new Vector3(-18.8f, 10.15f, -30);
+                break;
+            case "cléDroite":
+                col.transform.position = new Vector3(-16.6f, 10.15f, -30);
+                break;
+            case "cléGauche":
+                col.transform.position = new Vector3(-17.3f, 10.15f, -30);
+                break;
+            default:
+                break;
+                
+        }
+
+    }
+}
